09c4cfd4c8443bce80887f1d25e2f7e12aba1001
yavel <yavelwork@gmail.com>
2023-05-10 18:24:31 -0300

    php

--- php/subscripcion.php
+++ php/subscripcion.php
@@ -1,19 +1,21 @@
 <?php
-$idmail = $_POST['direcmail'];
+if (isset($_POST['submit'])) {
+	$idmail = $_POST['direcmail'];
 
 
 
-$email_form = 'yavelwork@gmail.com';
-$email_subject = "Nueva Subscripcion YavelArt ES";
-$email_body = "Recibiste una nueva subscripcion de: $idmail".
+	$email_form = 'yavelwork@gmail.com';
+	$email_subject = "Nueva Subscripcion YavelArt ES";
+	$email_body = "Recibiste una nueva subscripcion de: $idmail".
 
-$to = "yavelwork@gmail.com";
-$headers = "From: $email_form \r\n";
+	$to = "yavelwork@gmail.com";
+	$headers = "From: $email_form \r\n";
 
-mail($to,$email_subject,$email_body,$headers);
+	mail($to,$email_subject,$email_body,$headers);
 
-	print" <script languaje='JavaScript'>
-	alert('Subscripto! Recibiras notificaciones al mail ingresado.');
-	window.location.href='https://yavelrico.github.io/YavelArt/';
-	</script>";
+		print" <script languaje='JavaScript'>
+		alert('Subscripto! Recibiras notificaciones al mail ingresado.');
+		window.location.href='https://yavelrico.github.io/YavelArt/';
+		</script>";
+}
 ?>
