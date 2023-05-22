3dbdc43624186177275e5ee9f8b42475af038472
yavel <yavelwork@gmail.com>
2023-05-22 19:02:09 -0300

    modificaciones

---
+++ js/main.js
@@ -0,0 +1,70 @@
+$(function() {
+
+	'use strict';
+
+	// Form
+
+	var contactForm = function() {
+
+		if ($('#contactForm').length > 0 ) {
+			$( "#contactForm" ).validate( {
+				rules: {
+					fname: "required",
+					lname: "required",
+					email: {
+						required: true,
+						email: true
+					},
+					message: {
+						required: false,
+						minlength: 1
+					}
+				},
+				messages: {
+					fname: "Por Favor ingrese su nombre",
+					lname: "Por Favor ingrese su apellido",
+					email: "Por Favor ingrese un mail válido",
+				},
+				/* submit via ajax */
+				submitHandler: function(form) {		
+					var $submit = $('.submitting'),
+						waitText = 'Submitting...';
+
+					$.ajax({   	
+				      type: "POST",
+				      url: "php/send-email.php",
+				      data: $(form).serialize(),
+
+				      beforeSend: function() { 
+				      	$submit.css('display', 'block').text(waitText);
+				      },
+				      success: function(msg) {
+		               if (msg == 'OK') {
+		               	$('#form-message-warning').hide();
+				            setTimeout(function(){
+		               		$('#contactForm').fadeOut();
+		               	}, 1000);
+				            setTimeout(function(){
+				               $('#form-message-success').fadeIn();   
+		               	}, 1400);
+			               
+			            } else {
+			               $('#form-message-warning').html(msg);
+				            $('#form-message-warning').fadeIn();
+				            $submit.css('display', 'none');
+			            }
+				      },
+				      error: function() {
+				      	$('#form-message-warning').html("Something went wrong. Please try again.");
+				         $('#form-message-warning').fadeIn();
+				         $submit.css('display', 'none');
+				      }
+			      });    		
+		  		}
+				
+			} );
+		}
+	};
+	contactForm();
+
+});
