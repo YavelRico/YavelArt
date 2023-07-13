fdb503deea9c29b808dedbdcd0ce641533341c05
yavel <yavelwork@gmail.com>
2023-07-12 21:38:02 -0300

    personajes

--- js/javascript.js
+++ js/javascript.js
@@ -2,30 +2,31 @@
 function myFunction() {
   var x = document.getElementById("topnav");
   if (x.className === "topnav") {
-	x.className += " responsive";
+    x.className += " responsive";
   } else {
-	x.className = "topnav";
+    x.className = "topnav";
   }
 }
 
 /*Fade-in Animation*/
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
+}
 
 window.addEventListener("scroll", reveal);
 
 // To check the scroll position on the page load
 reveal();
+
